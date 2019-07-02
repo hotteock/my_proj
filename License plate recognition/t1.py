a34c70fed389f75a2b840558c87bf40927a65cd9
hotteock <38280229+hotteock@users.noreply.github.com>
2019-07-02 23:13:27 +0900

    Add files via upload

--- License plate recognition/t1.py
+++ License plate recognition/t1.py
@@ -5,7 +5,7 @@ from  PIL import Image
 
 class num_rcg:
     def Ext_num(self):
-        img = cv2.imread('num1.jpg', cv2.IMREAD_COLOR)
+        img = cv2.imread('num3.jpg', cv2.IMREAD_COLOR)
         copy_img = img.copy()
         img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -30,6 +30,43 @@ class num_rcg:
             if(aspect_ratio >= 0.1) and (aspect_ratio <= 3.0) and (rect_area >= 70) and(rect_area <= 850):
                 cv2.rectangle(img,(x,y), (x+w, y+h), (0, 255, 0), 1)
                 boxes.append(cv2.boundingRect(cnt))
+
+        #sorted(boxes)
+
+        for i in range(len(boxes)):  ##Buble Sort on python
+            for j in range(len(boxes) - (i + 1)):
+                if boxes[j][0] > boxes[j + 1][0]:
+                    temp = boxes[j]
+                    boxes[j] = boxes[j + 1]
+                    boxes[j + 1] = temp
+
+        f_count = 0
+        select = 0
+        plate_width = 0
+
+        for i in range(len(boxes)):
+            count = 0
+            for j in range(i+1, (len(boxes)-1)):
+                dx = abs(boxes[j+1][0]-boxes[i][0])
+                if dx>150:
+                    break
+                dy=abs(boxes[j+1][1]-boxes[i][1])
+                if dx ==0:
+                    dx=1
+                if dy==0:
+                    dy=1
+                grad = float(dy)/float(dx)
+                if grad<0.25:
+                    count = count+1
+            if count>f_count:
+                select = i
+                f_count = count
+                plate_width = dx
+
+        number_plate = copy_img[boxes[select][1] - 10:boxes[select][3] + boxes[select][1] + 20,
+                       boxes[select][0] - 10:140 + boxes[select][0]]
+
+        cv2.imwrite('tt.jpg', number_plate)
 
         cv2.imwrite('tst.jpg', img)
         a=1
